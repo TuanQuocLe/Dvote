abbfb475ca4af9640710b2c4c6f5cc301bcbac1a
Tuan Le <tuanqcle@gmail.com>
2021-12-07 04:36:25 -0700

    fix web-interface

--- src/App.js
+++ src/App.js
@@ -3,24 +3,34 @@ import './App.css';
 function App() {
   return (
     <div className='app' >
-      <h1>Vote 2020</h1>
+      <h1>Election 2020</h1>
       <div className='body-box'>
         <div>
           <h3>RANKING</h3>
           <ul>
-            CANDIDATE RANK GOES HERE!
           </ul>
         </div>
         <div>
-          <h3>CANDIDATE</h3>
-          <ul>LIST OF CANDIDATE GOES RIGHT HERE!!!</ul>
+          <h3>CANDIDATES</h3>
+          <ul>
+
+          </ul>
         </div>
-      </div>
-      <form className='add-form'>
-        <label>Add candidate</label>
-        <input/>
-        <input type='submit'/>
-      </form>
+
+        <div className='add-form'>
+            <h3>Add candidate!</h3>
+            <input id='input'/>
+            <button className='submit-btn'>Submit</button>
+        </div>
+        <div className='finalize'>
+          <h3>Winner</h3>
+            <label className='winner-label'>
+              Finalize Election!
+            </label>
+            <button className='submit-btn'>Submit</button>
+          </div>
+        </div>
+      
     </div>
   );
 }
