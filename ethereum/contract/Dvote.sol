5f431b998ba67c3dffdc843b6b21d0d2b17c9f5a
Tuan Le <tuanqcle@gmail.com>
2021-12-07 03:20:52 -0700

    adding the contract file

---
+++ ethereum/contract/Dvote.sol
@@ -0,0 +1,62 @@
+pragma solidity ^0.8.10;
+
+contract Dvote {
+    address public manager;
+    string public contractName;
+    modifier managerOnly() {
+        require(msg.sender == manager);
+        _;
+    }
+    CandidateInfo[] public Candidates;
+    mapping(address => bool) public voted;
+
+    struct CandidateInfo {
+        string  name;
+        uint voteCount;
+    }
+
+
+
+    constructor ( string memory _name) {
+        manager = msg.sender;
+        contractName = _name;
+    }
+
+    function addCandidate ( string memory newCandidateName) public managerOnly {
+        // CandidateInfo memory newCandidate = CandidateInfo({
+        //     name: newCandidateName,
+        //     voteCount:0
+        // });
+
+        CandidateInfo storage newCandidate = Candidates.push();
+        newCandidate.name = newCandidateName;
+    }
+
+    function vote (uint index) public {
+        require(!voted[msg.sender]);
+        CandidateInfo storage candidate = Candidates[index];
+        candidate.voteCount++;
+        voted[msg.sender] = true;
+    } 
+
+    function finalize()  public view managerOnly returns (string memory) {
+        uint mostVotes = 0;
+        string memory winner = ' NO WINNER AT THIS TIME';
+        for (uint i = 0; i < Candidates.length; i++) {
+            if (Candidates[i].voteCount > 0 && Candidates[i].voteCount >= mostVotes){
+                if (Candidates[i].voteCount > mostVotes) {
+                    mostVotes = Candidates[i].voteCount;
+                    winner = Candidates[i].name; 
+                } else {
+                    mostVotes = Candidates[i].voteCount;
+                    winner = 'No winner at this time';
+                }
+
+                
+                
+
+            }
+        }
+        return winner;
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app' >
+      <h1>Vote 2020</h1>
+      <div className='body-box'>
+        <div>
+          <h3>RANKING</h3>
+          <ul>
+            CANDIDATE RANK GOES HERE!
+          </ul>
+        </div>
+        <div>
+          <h3>CANDIDATE</h3>
+          <ul>LIST OF CANDIDATE GOES RIGHT HERE!!!</ul>
+        </div>
+      </div>
+      <form className='add-form'>
+        <label>Add candidate</label>
+        <input/>
+        <input type='submit'/>
+      </form>
     </div>
   );
 }
